340b2fb2b56b6157b9063f2a26485e18e3dbb527
RPapagiannis <rpapagiannis@icloud.com>
2024-06-09 18:46:44 +0300

    Define a function that determines whether a year is a leap year

--- 06_leapYears/test.js
+++ 06_leapYears/test.js
@@ -1,4 +1,5 @@
-const leapYears = function(year) {
+function leapYears(year) {
+    
     if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
         return true;
     }
@@ -11,7 +12,6 @@ const leapYears = function(year) {
     else {
         return false;
     }
-};
+}
 
-// Do not edit below this line
-module.exports = leapYears;
+console.log(leapYears(1840));
